fd1ee8c86223b07cd4f50a156eae4c99a981e872
Khoa Nguyen <khoanguyen@Khoas-MacBook-Pro.local>
2017-10-30 09:10:37 -0700

    Reading in csv file

--- src/test.py
+++ src/test.py
@@ -1,1 +1,11 @@
-from imdb import IMDb
+import csv
+with open('../movie_metadata.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    i = 0
+    movies = []
+    for row in reader:
+        movie = (row['director_name'], row['num_critic_for_reviews'])
+        # print(movie)
+        movies.append(movie)
+    print(movies)
+    
